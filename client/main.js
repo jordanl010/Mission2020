d85c2b1c3454094e85c8b5acc327369629f0e31c
Jordan Lumley <Jordan Lumley>
2016-08-26 17:33:33 -0400

    fixing mobile site

--- client/main.js
+++ client/main.js
@@ -1,25 +1,49 @@
 'use strict';
 
+'use strict';
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        angular.module('mission2020App', [
+    'ngRoute'
+    ]).config(['$routeProvider', '$httpProvider', '$locationProvider', function ($routeProvider, $httpProvider, $locationProvider) {
 
-angular.module('mission2020App', [
-    'angular-parallax',
-'ngRoute'
-]).config(['$routeProvider', '$httpProvider', '$locationProvider', function ($routeProvider, $httpProvider, $locationProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl',
+                access: {
+                    restricted: false
+                }
+            }).otherwise({
+                redirectTo: '/',
+                access: {
+                    restricted: false
+                }
+            });
 
-    $routeProvider.when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'HomeCtrl',
-        access: {
-            restricted: false
-        }
-    }).otherwise({
-        redirectTo: '/',
-        access: {
-            restricted: false
-        }
-    });
+            $locationProvider.html5Mode({
+                enabled: true
+            });
+    }]);
+} else {
+    angular.module('mission2020App', [
+        'angular-parallax',
+    'ngRoute'
+    ]).config(['$routeProvider', '$httpProvider', '$locationProvider', function ($routeProvider, $httpProvider, $locationProvider) {
 
-    $locationProvider.html5Mode({
-        enabled: true
-    });
-}]);
+            $routeProvider.when('/', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl',
+                access: {
+                    restricted: false
+                }
+            }).otherwise({
+                redirectTo: '/',
+                access: {
+                    restricted: false
+                }
+            });
+
+            $locationProvider.html5Mode({
+                enabled: true
+            });
+    }]);
+}
